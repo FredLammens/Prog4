e4167d5a9e580b73f53a7d00fd6809a545e4871a
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-03-27 17:42:38 +0100

    Strategy oef begonnen

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// categorie Behavioral => wisselen van ene implementatie naar een andere implementatie d.m.v interfaces
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
